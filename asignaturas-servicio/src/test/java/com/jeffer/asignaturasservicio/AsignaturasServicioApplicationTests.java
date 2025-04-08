450661ffd46050410f3f55a85fcfc1707e3523a2
JefferPinzon <148707907+JefferPinzon@users.noreply.github.com>
2025-04-08 16:43:35 -0500

    Revert "Merge branch 'main' of https://github.com/JefferPinzon/microservicios-sistema-educativo"
    This reverts commit 835d319bed21631666cbdd262207cdbf4ace763e, reversing
    changes made to 292393e4f9131040672532ee1ee40590e5c06866.

---
+++ asignaturas-servicio/src/test/java/com/jeffer/asignaturasservicio/AsignaturasServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffer.asignaturasservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsignaturasServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
