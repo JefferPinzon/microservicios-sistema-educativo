292393e4f9131040672532ee1ee40590e5c06866
JefferPinzon <148707907+JefferPinzon@users.noreply.github.com>
2025-04-07 19:11:15 -0500

    eureka server
    eureka server

---
+++ discovery-microservice/src/test/java/com/microsercice/discovery_microservice/DiscoveryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsercice.discovery_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
